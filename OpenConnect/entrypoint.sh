54e25a8637f4fccdd45a847673e78023f73c1563
anarchist117 <146730762+anarchist117@users.noreply.github.com>
2024-11-21 00:55:37 +0300

    Update entrypoint.sh

--- OpenConnect/entrypoint.sh
+++ OpenConnect/entrypoint.sh
@@ -6,6 +6,10 @@ iptables -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
 if [ ! -f /etc/ocserv/ocserv.conf ]; then
   echo "Create ocserv.conf"
   cp /ocserv.conf /etc/ocserv/ocserv.conf
+  fullchain=/etc/ocserv/le/live/${Domain}/fullchain.pem
+  privkey=/etc/ocserv/le/live/${Domain}/privkey.pem
+  sed -i "s|^server-cert =.*|server-cert = ${fullchain}|" /etc/ocserv/ocserv.conf
+  sed -i "s|^server-key =.*|server-key = ${privkey}|" /etc/ocserv/ocserv.conf
 fi
 
 if [ ! -d /etc/ocserv/ssl/ ]; then
@@ -40,9 +44,9 @@ EOF
   certtool --generate-self-signed --load-privkey ca.key --template ca.tmpl --outfile ca.pem > /dev/null 2>&1
   mkdir -p /etc/ocserv/users
   cp /etc/ocserv/ssl/ca.pem /etc/ocserv/users/ca.cer
-
-  echo ""
 fi
+
+echo ""
 
 ocserv -c /etc/ocserv/ocserv.conf -f &
 cron -f

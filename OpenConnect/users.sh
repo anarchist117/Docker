fc25957ab914e5b1b775a6cd6ab5a58fa19b395e
anarchist117 <146730762+anarchist117@users.noreply.github.com>
2024-11-21 00:54:38 +0300

    Update users.sh

--- OpenConnect/users.sh
+++ OpenConnect/users.sh
@@ -16,8 +16,8 @@ if [ -f /etc/ocserv/users.txt ]; then
   while IFS= read -r user; do
     if ! grep -q "^$user" "/etc/ocserv/passwd" 2> /dev/null; then
       yes "$user" | ocpasswd -c /etc/ocserv/passwd "$user"
-      sed -i "s/^cn =.*/cn = \"$user\"/" "/etc/ocserv/ssl/user.tmpl"
-      sed -i "s/^uid =.*/uid = \"$user\"/" "/etc/ocserv/ssl/user.tmpl"
+      sed -i "s|^cn =.*|cn = $user|" /etc/ocserv/ssl/user.tmpl
+      sed -i "s|^uid =.*|uid = $user|" /etc/ocserv/ssl/user.tmpl
       certtool --generate-privkey --outfile user.key > /dev/null 2>&1
       certtool --generate-certificate --load-ca-certificate ca.pem --load-ca-privkey ca.key --template user.tmpl --load-privkey user.key --outfile user.pem > /dev/null 2>&1
       yes "$user" | certtool --to-p12 --load-certificate user.pem --load-privkey user.key --pkcs-cipher 3des-pkcs12 --outfile /etc/ocserv/users/"$user.p12" --outder > /dev/null 2>&1

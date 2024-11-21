b9ab88e06f616483a4b9629c888591cef5455281
anarchist117 <146730762+anarchist117@users.noreply.github.com>
2024-11-21 13:35:26 +0300

    Update entrypoint.sh

--- OpenConnect/entrypoint.sh
+++ OpenConnect/entrypoint.sh
@@ -5,11 +5,60 @@ iptables -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
 
 if [ ! -f /etc/ocserv/ocserv.conf ]; then
   echo "Create ocserv.conf"
-  cp /ocserv.conf /etc/ocserv/ocserv.conf
-  fullchain=/etc/ocserv/le/live/${Domain}/fullchain.pem
-  privkey=/etc/ocserv/le/live/${Domain}/privkey.pem
-  sed -i "s|^server-cert =.*|server-cert = ${fullchain}|" /etc/ocserv/ocserv.conf
-  sed -i "s|^server-key =.*|server-key = ${privkey}|" /etc/ocserv/ocserv.conf
+  cat > /etc/ocserv/ocserv.conf <<EOF
+auth = "certificate"
+tcp-port = 443
+
+run-as-user = ocserv
+run-as-group = ocserv
+socket-file = /run/ocserv-socket
+chroot-dir = /var/lib/ocserv
+server-cert = /etc/ocserv/le/live/${Domain}/fullchain.pem
+server-key = /etc/ocserv/le/live/${Domain}/privkey.pem
+ca-cert = /etc/ocserv/ssl/ca.pem
+isolate-workers = true
+
+max-clients = 100
+max-same-clients = 1
+rate-limit-ms = 100
+server-stats-reset-time = 604800
+
+keepalive = 32400
+dpd = 90
+mobile-dpd = 1800
+switch-to-tcp-timeout = 25
+try-mtu-discovery = true
+
+cert-user-oid = 0.9.2342.19200300.100.1.1
+tls-priorities = "NORMAL:%SERVER_PRECEDENCE:%COMPAT:-VERS-SSL3.0:-VERS-TLS1.0:-VERS-TLS1.1:-VERS-TLS1.3"
+
+auth-timeout = 240
+min-reauth-time = 300
+max-ban-score = 80
+ban-reset-time = 1200
+cookie-timeout = 300
+deny-roaming = false
+rekey-time = 172800
+rekey-method = ssl
+use-occtl = true
+pid-file = /run/ocserv.pid
+log-level = 1
+device = vpns
+predictable-ips = true
+
+ipv4-network = 192.168.168.0
+ipv4-netmask = 255.255.255.0
+tunnel-all-dns = true
+dns = 8.8.8.8
+dns = 9.9.9.9
+ping-leases = false
+no-route = 10.0.0.0/8
+no-route = 172.16.0.0/12
+no-route = 192.168.0.0/16
+cisco-client-compat = true
+dtls-legacy = true
+client-bypass-protocol = false
+EOF
 fi
 
 if [ ! -d /etc/ocserv/ssl/ ]; then
